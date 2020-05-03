0f02d8f1fc4708c12c8dc23e31980762469a59c6
David Kossoglyad <david@logoui.co.il>
2020-05-03 13:29:18 +0300

    feat(test): make tests as a real tests

--- test/features/step-definitions/attributes-color.steps.ts
+++ test/features/step-definitions/attributes-color.steps.ts
@@ -6,7 +6,7 @@ const feature = loadFeature('./test/features/attributes-color.feature');
 
 defineFeature(feature, test => {  
   let component: ComponentTester<any>;
-  let model = {color: 'green'};
+  let model = { color: 'Yellow' };
 
   afterEach(() => {
     if (component) {
@@ -15,35 +15,66 @@ defineFeature(feature, test => {
     }
   });
 
-test('It sets font color', ({
+  test('It sets font color with bound value', ({
     given,
     when,
+    and,
     then
-    }) => {      
+  }) => {
 
-    
-    given('I search for cats', async () => {
-      //
+    given(/^I have the model with property color set to value (.*)$/, async (arg0) => {
+      model.color = arg0;
     });
-
-    when('I am on google', async () => {
+  
+    when(/^I have the view composed with (.*) tag$/, async (arg0) => {
       component = StageComponent
         .withResources('attributes/color')
         .inView(`<p color.bind="color"></p>`)
-        .boundTo(model);      
+        .boundTo(model);  
     });
+  
+    and(/^I set the color value to (.*)$/, async (arg0) => {
+      model.color = arg0;
+    });
+  
+    then(/^I expect to see the (.*) background$/, async (arg0) => {
+      await component.create(bootstrap).then(() => {
+      
+        const view = <any>component.element;
+        expect(view.style.color).toBe(arg0);
 
-    then('I should see cats', async ()=> {
-           
-        await component.create(bootstrap).then(() => {
-        
-            const view = <any>component.element;
-            expect(view.style.color).toBe('green');
-    
-          }).catch((e: any) => {
-            fail(e);
-          });
+      }).catch((e: any) => {
+        fail(e);
+      });
     });
-        
+  });
+
+
+  test('It sets font color with hard coded value', ({
+    given,
+    when,
+    then
+  }) => {
+    given(`I have the view composed with "p" tag`, () => {
+      //
+    });
+  
+    when(/^I set the color value to (.*)$/, async (arg0) => {
+      component = StageComponent
+        .withResources('attributes/color')
+        .inView(`<p color="${arg0}"></p>`);
+    });
+  
+    then(/^I expect to see (.*) background$/, async (arg0) => {
+
+      await component.create(bootstrap).then(() => {
+      
+        const view = <any>component.element;
+        expect(view.style.color).toBe(arg0);
+
+      }).catch((e: any) => {
+        fail(e);
+      });
+    });
   });
 });
